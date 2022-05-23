c309326d1f48f0bd0f69a2768e8df07fd655bb8b
pinglarin <pinglarin@gmail.com>
2022-05-23 13:49:10 +0700

    ocr video

--- ocr-speech/main.py
+++ ocr-speech/main.py
@@ -1,3 +1,4 @@
+from genericpath import exists
 from fastapi import FastAPI
 
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
@@ -12,6 +13,10 @@ import os
 from PIL import Image
 import sys
 import time
+import cv2
+import io
+import numpy as np
+from starlette.responses import StreamingResponse
 
 app = FastAPI()
 
@@ -38,11 +43,95 @@ def recognize_from_microphone():
             ret += "Did you set the speech resource key and region values?" + "\n\n"
 
     return ret
-    
 
+@app.get("/displayFrame/{frameNum}")
+async def displayFrame(frameNum):
+    frameNum = int(frameNum)
+    vidcap = cv2.VideoCapture('/Users/pinglarin/Documents/script_code/imaginecup-2023/ocr-speech/comVidCut_trim.mp4')
+    vidcap.set(1, frameNum-1)
+    res, frameImg = vidcap.read()
+
+    res, im_png = cv2.imencode(".png", frameImg)
+    return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+
+@app.get("/frameOCR/{frameNum}")
+async def frameOCR(frameNum):
+    frameNum = int(frameNum)
+    # frameNum = 100
+    vidcap = cv2.VideoCapture('/Users/pinglarin/Documents/script_code/imaginecup-2023/ocr-speech/comVidCut_trim.mp4')
+    
+    # amount_of_frames = vidcap.get(cv2.CV_CAP_PROP_FRAME_COUNT)
+
+    vidcap.set(1, frameNum-1)
+    res, frameImg = vidcap.read()
+    # print(frameImg, "A")
+    is_success, buffer = cv2.imencode(".jpg", frameImg)
+    # print(buffer, "B")
+    
+    io_buf = io.BytesIO(buffer)
+    # print(io_buf, "C")
+    b_handle = io_buf
+    b_handle.seek(0)
+    # print(b_handle, "C.5")
+    b_br = io.BufferedReader(b_handle)
+    # print(b_br, "C.9")
+
+    ret = localocr(b_br)
+
+    return ret
+
+    # res, im_png = cv2.imencode(".png", frameImg)
+    # return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+
+@app.get("/testimg")
+async def testimg():
+    
+    frameImg=cv2.imread('testimg.png')
+    # cv2.imshow(frameImg)
+    res, im_png = cv2.imencode(".png", frameImg)
+    im_png.seek(0)
+    return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+
+@app.get("/localOCR")
+def localocr(img): # async
+    subscription_key = "37cf6d8217354a25b5bad0cc9a738599"
+    endpoint = "https://ocr-computer-vision-imaginecup-2023.cognitiveservices.azure.com/"
+
+    computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+    print("===== Read File - local =====")
+
+    # read_image_path = "/Users/pinglarin/Documents/script_code/imaginecup-2023/ocr-speech/testimg.png"
+    # read_image = open(read_image_path, "rb")
+    print(img)
+    # Call API with image and raw response (allows you to get the operation location)
+    read_response = computervision_client.read_in_stream(img, raw=True)
+    # Get the operation location (URL with ID as last appendage)
+    read_operation_location = read_response.headers["Operation-Location"]
+    # Take the ID off and use to get results
+    operation_id = read_operation_location.split("/")[-1]
+
+    # Call the "GET" API and wait for the retrieval of the results
+    while True:
+        read_result = computervision_client.get_read_result(operation_id)
+        if read_result.status.lower () not in ['notstarted', 'running']:
+            break
+        print ('Waiting for result...')
+        time.sleep(10)
+
+    ret = ""
+
+    # Print the detected text, line by line
+    if read_result.status == OperationStatusCodes.succeeded:
+        for text_result in read_result.analyze_result.read_results:
+            for line in text_result.lines:
+                ret += ''.join(line.text) 
+                ret += ' '.join(str(v) for v in line.bounding_box) 
+
+    return ret
 
 @app.get("/sampleOCR")
-async def ocr():
+def ocr(img): # async
     subscription_key = "37cf6d8217354a25b5bad0cc9a738599"
     endpoint = "https://ocr-computer-vision-imaginecup-2023.cognitiveservices.azure.com/"
 
@@ -50,10 +139,12 @@ async def ocr():
 
     print("===== Read File - remote =====")
     # Get an image with text
+    # if 'img' in locals():
     read_image_url = "https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/master/articles/cognitive-services/Computer-vision/Images/readsample.jpg"
 
     # Call API with URL and raw response (allows you to get the operation location)
-    read_response = computervision_client.read(read_image_url,  raw=True)
+    read_response = computervision_client.read(read_image_url,  raw=True) # _in_stream
+    print(computervision_client, read_response.headers)
 
     # Get the operation location (URL with an ID at the end) from the response
     read_operation_location = read_response.headers["Operation-Location"]
