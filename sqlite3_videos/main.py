58f6253db5556d5feb889d492765e20a135d4241
Jidapaminnie <jidapaminnie@gmail.com>
2022-06-03 18:03:50 +0700

    FastAPI Video streaming

--- sqlite3_videos/main.py
+++ sqlite3_videos/main.py
@@ -10,6 +10,12 @@ import uvicorn
 import uuid
 from typing import Optional, Type
 
+from pathlib import Path
+from fastapi import FastAPI
+from fastapi import Request, Response
+from fastapi import Header
+from fastapi.templating import Jinja2Templates
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -124,6 +130,27 @@ def update_hero(vuuid: str, video: schemas.VideoUpdate = Depends(VideoUpdate.as_
     db.commit()
     db.refresh(db_vdo)
     return db_vdo
+
+
+templates = Jinja2Templates(directory="templates")
+CHUNK_SIZE = 1024*1024
+video_path = Path("videos/comVidCutMP4.mp4")
+
+
+@app.get("/streamvideo")
+async def video_endpoint(range: str = Header(None)):
+    start, end = range.replace("bytes=", "").split("-")
+    start = int(start)
+    end = int(end) if end else start + CHUNK_SIZE
+    with open(video_path, "rb") as video:
+        video.seek(start)
+        data = video.read(end - start)
+        filesize = str(video_path.stat().st_size)
+        headers = {
+            'Content-Range': f'bytes {str(start)}-{str(end)}/{filesize}',
+            'Accept-Ranges': 'bytes'
+        }
+        return Response(data, status_code=206, headers=headers, media_type="comVidCut/mp4")
 
 #ORIGINAL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # @app.patch("/updatevideo/{id}", response_model=schemas.VideoReturn) #not done
